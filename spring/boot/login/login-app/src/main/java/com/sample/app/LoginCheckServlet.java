32640f57c136e6dd9c22c3d304d2272ccd6f34b3
Wangxy <xtwxy@hotmail.com>
2018-08-13 17:41:55 +0800

    add output of login info.

--- spring/boot/login/login-app/src/main/java/com/sample/app/LoginCheckServlet.java
+++ spring/boot/login/login-app/src/main/java/com/sample/app/LoginCheckServlet.java
@@ -1,36 +1,51 @@
 package com.sample.app;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @WebServlet(name = "loginCheckServlet", urlPatterns = {"/login-check"})
 public class LoginCheckServlet extends HttpServlet {
 
-  Logger log = LoggerFactory.getLogger(LoginCheckServlet.class);
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse rsp) throws IOException, ServletException {
+		doPost(req, rsp);
+	}
 
-  @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse rsp) throws IOException, ServletException {
-    doPost(req, rsp);
-  }
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse rsp) throws IOException, ServletException {
+		rsp.setContentType("text/plain");
+		if (null == req.getUserPrincipal()) {
+			rsp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			PrintWriter pw = rsp.getWriter();
+			pw.println("No logged in.");
+			pw.close();
+		} else {
+			rsp.setStatus(HttpServletResponse.SC_OK);
+			PrintWriter pw = rsp.getWriter();
+			pw.println(String.format("AuthType: %s", req.getAuthType()));
+			pw.println(String.format("UserPrincipal: %s", req.getUserPrincipal()));
+			pw.println(String.format("UserPrincipal Class: %s", req.getUserPrincipal().getClass().getName()));
+			pw.println(String.format("HttpServletRequest Class: %s", req.getClass().getName()));
+			pw.println(String.format("is admin role: %s", req.isUserInRole("admin")));
+			pw.println(requestCookies(req));
+			pw.close();
+		}
+	}
 
-  @Override
-  public void doPost(HttpServletRequest req, HttpServletResponse rsp) throws IOException, ServletException {
-    if(null == req.getUserPrincipal()) {
-      rsp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-    } else {
-      log.info(String.format("AuthType: %s", req.getAuthType()));
-      log.info(String.format("UserPrincipal: %s", req.getUserPrincipal()));
-      log.info(String.format("UserPrincipal: %s", req.getUserPrincipal().getClass().getName()));
-      log.info(String.format("request: %s", req.getClass().getName()));
-      log.info(String.format("is admin role: %s", req.isUserInRole("admin")));
-      rsp.setStatus(HttpServletResponse.SC_OK);
-    }
-  }
+	private String requestCookies(HttpServletRequest req) {
+		Cookie[] cookies = req.getCookies();
+		StringBuilder cookieString = new StringBuilder();
+		cookieString.append("Cookie: ");
+		for (Cookie c : cookies) {
+			cookieString.append(String.format("%s=%s, maxAge=%s\n", c.getName(), c.getValue(), c.getMaxAge()));
+		}
+		return cookieString.toString();
+	}
 }
