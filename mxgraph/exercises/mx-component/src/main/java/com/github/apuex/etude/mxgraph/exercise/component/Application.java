ed2e314bf48f54b9cab61f407e4d2e433067d37c
Wangxy <xtwxy@hotmail.com>
2018-09-30 10:59:31 +0800

    init mxgraph component test project.

---
+++ mxgraph/exercises/mx-component/src/main/java/com/github/apuex/etude/mxgraph/exercise/component/Application.java
@@ -0,0 +1,21 @@
+package com.github.apuex.etude.mxgraph.exercise.component;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(Application.class);
+  }
+}
---
+++ mxgraph/exercises/mx-component/src/main/java/com/github/apuex/etude/mxgraph/exercise/component/WebjarsJsController.java
@@ -0,0 +1,38 @@
+package com.github.apuex.etude.mxgraph.exercise.component;
+
+import static org.webjars.RequireJS.generateSetupJavaScript;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.webjars.WebJarAssetLocator;
+
+@RestController
+public class WebjarsJsController {
+  private final static Logger logger = LoggerFactory.getLogger(WebjarsJsController.class);
+
+  @ResponseBody
+  @RequestMapping(value = "/webjarsjs", produces = "application/javascript")
+  public String webjarjs(HttpServletRequest r) {
+    Map<String, String> webJars = new WebJarAssetLocator().getWebJars();
+    webJars.forEach((k, v) -> logger.info("{} => {}", k, v));
+    List<String> prefixes = new LinkedList<>();
+    String prefix = String.format("%s/webjars/", r.getContextPath());
+    prefixes.add(prefix);
+    String mxBasePath = String.format("var mxBasePath=\"%smxgraph-client/3.9.8\";", prefix);
+    String baseUri = String.format("var BASE_URI = %s;", r.getContextPath());
+    return String.format("%s\n%s\n%s\n",
+    		generateSetupJavaScript(prefixes, webJars),
+    		mxBasePath,
+        baseUri
+    		);
+  }
+}
