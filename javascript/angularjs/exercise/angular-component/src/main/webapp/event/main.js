b48dce0a27ecdee88e906fc6212f9098383e0b09
Wangxy <xtwxy@hotmail.com>
2018-10-06 18:27:17 +0800

    add mouse event trap.

--- javascript/angularjs/exercise/angular-component/src/main/webapp/event/main.js
+++ javascript/angularjs/exercise/angular-component/src/main/webapp/event/main.js
@@ -1,6 +1,8 @@
 require(['angular'], function(angular) {
   var app = angular.module('eventApp', []);
   app.controller("eventController", function ($scope, $window) {
+    $scope.enter = false;
+    $scope.leave = true;
     $scope.DisplayMessage = function (value) {
       $scope.newPassword = value;
     };
