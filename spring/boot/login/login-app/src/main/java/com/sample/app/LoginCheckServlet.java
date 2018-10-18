db5a77e581d1b25f456bd9268754059d59991f2f
Wangxy <xtwxy@hotmail.com>
2018-10-18 13:58:19 +0800

    add autologin filter.

--- spring/boot/login/login-app/src/main/java/com/sample/app/LoginCheckServlet.java
+++ spring/boot/login/login-app/src/main/java/com/sample/app/LoginCheckServlet.java
@@ -13,87 +13,84 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "loginCheckServlet", urlPatterns = {"/login-check"})
 public class LoginCheckServlet extends HttpServlet {
 
-	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse rsp) throws IOException, ServletException {
-		doPost(req, rsp);
-	}
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse rsp) throws IOException, ServletException {
+    doPost(req, rsp);
+  }
 
-	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse rsp) throws IOException, ServletException {
-		rsp.setContentType("text/plain");
-		if (null == req.getUserPrincipal()) {
-			login(req, rsp);
-			if(null == req.getUserPrincipal()) {
-				unauthorizing(rsp);
-			} else {
-				authorized(req, rsp);
-			}
-		} else {
-			authorized(req, rsp);
-		}
-	}
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse rsp) throws IOException, ServletException {
+    rsp.setContentType("text/plain");
+    if (null == req.getUserPrincipal()) {
+      unauthorized(rsp);
+    } else {
+      authorized(req, rsp);
+    }
+  }
 
-	private void authorized(HttpServletRequest req, HttpServletResponse rsp) throws IOException {
-		rsp.setStatus(HttpServletResponse.SC_OK);
-		PrintWriter pw = rsp.getWriter();
-		pw.println(String.format("AuthType: %s", req.getAuthType()));
-		pw.println(String.format("UserPrincipal: %s", req.getUserPrincipal()));
-		pw.println(String.format("UserPrincipal Class: %s", req.getUserPrincipal().getClass().getName()));
-		pw.println(String.format("HttpServletRequest Class: %s", req.getClass().getName()));
-		pw.println(String.format("is admin role: %s", req.isUserInRole("admin")));
-		pw.println(requestCookies(req));
-		pw.close();
-	}
+  private void authorized(HttpServletRequest req, HttpServletResponse rsp) throws IOException {
+    rsp.setStatus(HttpServletResponse.SC_OK);
+    PrintWriter pw = rsp.getWriter();
+    pw.println(String.format("AuthType: %s", req.getAuthType()));
+    pw.println(String.format("UserPrincipal: %s", req.getUserPrincipal()));
+    pw.println(String.format("UserPrincipal Class: %s", req.getUserPrincipal().getClass().getName()));
+    pw.println(String.format("HttpServletRequest Class: %s", req.getClass().getName()));
+    pw.println(String.format("is admin role: %s", req.isUserInRole("admin")));
+    pw.println(requestCookies(req));
+    pw.close();
+  }
 
-	private void unauthorizing(HttpServletResponse rsp) throws IOException {
-		rsp.setStatus(HttpServletResponse.SC_OK);
-		PrintWriter pw = rsp.getWriter();
-		pw.println("No logged in.");
-		pw.close();
-	}
+  private void authorizing(HttpServletResponse rsp) throws IOException {
+    rsp.setStatus(HttpServletResponse.SC_OK);
+    PrintWriter pw = rsp.getWriter();
+    pw.println("No logged in.");
+    pw.close();
+  }
 
-	private void unauthorized(HttpServletResponse rsp) throws IOException {
-		rsp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		PrintWriter pw = rsp.getWriter();
-		pw.println("No logged in.");
-		pw.close();
-	}
+  private void unauthorized(HttpServletResponse rsp) throws IOException {
+    rsp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    rsp.addCookie(new Cookie("userName", "test"));
+    rsp.addCookie(new Cookie("passWord", "test"));
+    PrintWriter pw = rsp.getWriter();
+    pw.println("No logged in.");
+    pw.close();
+  }
 
-	private String requestCookies(HttpServletRequest req) {
-		Cookie[] cookies = req.getCookies();
-		StringBuilder cookieString = new StringBuilder();
-		cookieString.append("Cookie: ");
-		for (Cookie c : cookies) {
-			cookieString.append(String.format("%s=%s, maxAge=%s\n", c.getName(), c.getValue(), c.getMaxAge()));
-		}
-		return cookieString.toString();
-	}
+  private String requestCookies(HttpServletRequest req) {
+    Cookie[] cookies = req.getCookies();
+    StringBuilder cookieString = new StringBuilder();
+    cookieString.append("Cookie: ");
+    for (Cookie c : cookies) {
+      cookieString.append(String.format("%s=%s, maxAge=%s\n", c.getName(), c.getValue(), c.getMaxAge()));
+    }
+    return cookieString.toString();
+  }
 
-	private void login(HttpServletRequest req, HttpServletResponse rsp) {
-		System.out.println("not logged in - try login.");
-		Cookie[] cookies = req.getCookies();
-		String username = "test";
-		String password = "test";
-		if (null != cookies) {
-			for (Cookie c : cookies) {
-				System.out.printf("%s = %s\n", c.getName(), c.getValue());
-				if ("userName".equals(c.getName())) {
-					username = c.getValue();
-				}
-				if ("passWord".equals(c.getName())) {
-					password = c.getValue();
-				}
-			}
-			if (null != username) {
-				try {
-					req.login(username, password);
-					System.out.println("logged in.");
-				} catch (Exception ex) {
-					System.out.println("login failed.");
-				}
-			} else {
-				System.out.println("no username - skip login.");
-			}
-		}
-	}
+  private void login(HttpServletRequest req, HttpServletResponse rsp) {
+    System.out.println("not logged in - try login.");
+    Cookie[] cookies = req.getCookies();
+    String username = "test";
+    String password = "test";
+    if (null != cookies) {
+      for (Cookie c : cookies) {
+        System.out.printf("%s = %s\n", c.getName(), c.getValue());
+        if ("userName".equals(c.getName())) {
+          username = c.getValue();
+        }
+        if ("passWord".equals(c.getName())) {
+          password = c.getValue();
+        }
+      }
+      if (null != username) {
+        try {
+          req.login(username, password);
+          System.out.println("logged in.");
+        } catch (Exception ex) {
+          System.out.println("login failed.");
+        }
+      } else {
+        System.out.println("no username - skip login.");
+      }
+    }
+  }
 }
