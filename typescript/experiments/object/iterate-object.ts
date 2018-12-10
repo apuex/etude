66e033fd6da3862bcf280bf6e9ceffea69030649
Wangxy <xtwxy@hotmail.com>
2018-12-10 11:30:05 +0800

    add iterator over object with Object.keys(object).

--- typescript/experiments/object/iterate-object.ts
+++ typescript/experiments/object/iterate-object.ts
@@ -3,5 +3,10 @@ let object = {
   bizar: "rel"
 };
 
+console.log('for(var k in object) console.log(k, object[k]);');
 for(var k in object) console.log(k, object[k]);
+
+console.log('Object.keys(object).forEach(k => console.log(k, object[k]));');
+Object.keys(object).forEach(k => console.log(k, object[k]));
+
 
