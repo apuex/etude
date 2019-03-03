c9c9faf8f1daafdf03ad07e2e87490d3ecd21fa7
Wangxy <93236565@qq.com>
2019-03-03 22:43:38 +0800

    add state variable test.

--- oracle/java/procedure/simple-procedure/src/main/java/com/github/apuex/oracle/SimpleProcedure.java
+++ oracle/java/procedure/simple-procedure/src/main/java/com/github/apuex/oracle/SimpleProcedure.java
@@ -7,22 +7,43 @@ import java.sql.SQLException;
 
 /**
  * Hello world!
- *
  */
-public class SimpleProcedure
-{
-    public static void insert_into_table(int ID, String Name, String Email) throws SQLException {
+public class SimpleProcedure {
+    static int count = 0;
+
+    public static void insert_into_table(int id, String name, String email) throws SQLException {
 
         Connection DB = DriverManager.getConnection("jdbc:default:connection:");
 
-        String SQL ="INSERT INTO sampletable VALUES (?,?,?)";
+        String SQL = "INSERT INTO sampletable VALUES (?,?,?)";
 
         PreparedStatement cmd = DB.prepareStatement(SQL);
 
-        cmd.setInt(1, ID);
-        cmd.setString(2, Name );
-        cmd.setString(3, Email );
+        count += 1;
+        cmd.setInt(1, (id == 0 ? count : id));
+        cmd.setString(2, name);
+        cmd.setString(3, email);
+
         cmd.executeUpdate();
         cmd.close();
+
+    }
+
+    public static void update_from_table(int id, String name, String email) throws SQLException {
+
+        Connection DB = DriverManager.getConnection("jdbc:default:connection:");
+
+        String SQL = "UPDATE sampletable SET name = ?, email = ? WHERE id = ?";
+
+        PreparedStatement cmd = DB.prepareStatement(SQL);
+
+        count += 1;
+        cmd.setString(1, name);
+        cmd.setString(2, email);
+        cmd.setInt(3, id);
+
+        cmd.executeUpdate();
+        cmd.close();
+
     }
 }
