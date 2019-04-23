681ee21e17851dff05f299f3f9d34146164d49fc
Wangxy <xtwxy@hotmail.com>
2019-04-23 11:24:27 +0800

    reformat code.

--- playframework/websocket/app/controllers/HomeController.scala
+++ playframework/websocket/app/controllers/HomeController.scala
@@ -2,25 +2,24 @@ package controllers
 
 import akka.stream.scaladsl.{Flow, Sink, Source}
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 import scala.concurrent.Future
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+    * Create an Action to render an HTML page.
+    *
+    * The configuration in the `routes` file means that this method
+    * will be called when the application receives a `GET` request with
+    * a path of `/`.
+    */
   def index(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
@@ -39,7 +38,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     })
   }
 
-  def events(offset: Long=1): WebSocket =   WebSocket.accept[String, String] { request =>
+  def events(offset: Long = 1): WebSocket = WebSocket.accept[String, String] { request =>
     // Log events to the console
     val in = Sink.foreach[String](println)
     // Send a single 'Hello!' message and then leave the socket open
