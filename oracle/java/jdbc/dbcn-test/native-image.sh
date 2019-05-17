48548fd2d6c3764854fba28c68a5fc182c8b2f35
Wangxy <xtwxy@hotmail.com>
2019-05-17 22:16:13 +0800

    add native image configuration.

---
+++ oracle/java/jdbc/dbcn-test/native-image.sh
@@ -0,0 +1,19 @@
+native-image \
+    -H:+PrintAnalysisCallTree \
+    -H:EnableURLProtocols=http \
+    -H:NativeLinkerOption=-no-pie \
+    -H:+StackTrace \
+    -H:+ReportUnsupportedElementsAtRuntime \
+    -H:+ReportExceptionStackTraces \
+    -H:IncludeResources=.*.conf \
+    -H:IncludeResources=.*.properties \
+    -H:IncludeResources=.*.xml \
+    -H:IncludeResourceBundles=oracle.net.mesg.Message \
+    -H:ReflectionConfigurationFiles=graal/reflection-xml.json \
+    --delay-class-initialization-to-runtime=oracle.sql.LnxLibServer \
+    --delay-class-initialization-to-runtime=oracle.sql.LoadCorejava \
+    --delay-class-initialization-to-runtime=oracle.jdbc.driver.OracleDriver \
+    --delay-class-initialization-to-runtime=oracle.jdbc.driver.OracleTimeoutThreadPerVM \
+    --allow-incomplete-classpath \
+    -jar target/dbcn-example-1.0.0.jar 
+
