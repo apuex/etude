40038cbb85adc541053f34903c36884d3445beba
Wangxy <xtwxy@hotmail.com>
2019-06-14 10:51:05 +0800

    reformat output.

--- git/loc-changes/src/main/scala/com/github/apuex/locc/Main.scala
+++ git/loc-changes/src/main/scala/com/github/apuex/locc/Main.scala
@@ -7,8 +7,6 @@ import scala.io.Source
   *
   * package it to a runnable jar, or AOT-compile to native image ^_^
   *
-  * lines must be sorted first ^_^
-  *
   * Usage:
   *
   *   `git log --numstat | grep -i ^[0-9] | awk '{print $1 "," $2 "," $3}' | java -jar locc.jar`
@@ -35,15 +33,21 @@ object Main extends App {
     .filter(_.length == 3)  // just in case...
     .map(x => (x(2) -> (x(0).toLong, x(1).toLong)))
     .foldLeft(Map[String, (Long, Long)]())(sum)
-    .map(x => (x._1, (x._2._1, x._2._2, x._2._1 + x._2._2)))
+    .map(x => (x._1, (x._2._1, x._2._2)))
     .toSeq
     .sortBy(_._1)
 
-  updates.foreach(x => println(s"${x._2._1}\t${x._2._2}\t${x._2._3}\t${x._1}"))
+  updates.foreach(x => println(s"${x._2._1}\t${x._2._2}\t${x._2._1 + x._2._2}\t${x._1}"))
 
   val total = updates
     .map(_._2)
     .foldLeft((0L, 0L))((l, r) => (l._1 + r._1, l._2 + r._2))
 
-  println(s"lines of code changed: (+${total._1}, -${total._2}) => ${total._1 + total._2}.")
+  val net = (total._1 - total._2)
+  println(
+    s"""
+       |Summary of LOC changed:
+       |  (+${total._1}, -${total._2}) => ${net}) yield.
+       |  ${if(total._1 == 0) "n/a" else (net / total._1)} ratio.
+     """.stripMargin.trim)
 }
