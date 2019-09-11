8ed48457cf6a16851042e2efa0bf1cb54eadcb59
Wangxy <xtwxy@hotmail.com>
2019-09-11 14:40:24 +0800

    add readlink -f equivalent commands for macos.

---
+++ linux/shell/readlink-f/push.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+PRG=$(readlink -f $0)
+PRG_DIR=$(dirname "${PRG}")
+
+cd ${PRG_DIR}
+REPOS=$(ls -d */ | cut -f1 -d'/')
+
+for r in ${REPOS[*]}
+do
+    echo ${PRG_DIR}/${r}
+    cd ${PRG_DIR}/${r}
+    git push
+done
+
