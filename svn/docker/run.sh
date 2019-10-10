11e1ddfeb6e8cca6ed1240ad1ff769e21b2f6bf1
gaocuiqun <gaocuiqun@126.com>
2019-10-10 16:15:16 +0800

    add initial user, and change mount volume location.

--- svn/docker/run.sh
+++ svn/docker/run.sh
@@ -4,5 +4,5 @@ PRG=$(readlink -f $0)
 PRG_DIR=$(dirname "${PRG}")
 
 cd ${PRG_DIR}
-docker run --restart=always --name svn -d -v /home/wangxy/svn-backups/svn:/var/www/svn -p 9080:80 svn-server:1.0.0
+docker run --restart=always --name svn-server -d -v /home/svn-store/repositories:/var/www/svn -p 9080:80 svn-server:1.0.0
 
