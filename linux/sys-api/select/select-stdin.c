bbd901ee391b7f3e6879c11ca675a92dc4445bd2
Wangxy <93236565@qq.com>
2019-11-24 13:38:40 +0800

    add select test.

---
+++ linux/sys-api/select/select-stdin.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int
+main (void)
+{
+  fd_set rfds;
+  struct timeval tv;
+  int retval;
+
+  /* Watch stdin (fd 0) to see when it has input. */
+  while (1)
+    {
+
+      FD_ZERO (&rfds);
+      FD_SET (0, &rfds);
+
+      /* Wait up to five seconds. */
+
+      tv.tv_sec = 5;
+      tv.tv_usec = 0;
+
+      retval = select (1, &rfds, NULL, NULL, &tv);
+      /* Don't rely on the value of tv now! */
+
+      if (retval == -1)
+	perror ("select()");
+      else if (retval)
+	{
+	  /* FD_ISSET(0, &rfds) will be true. */
+
+	  char *line = NULL;
+	  size_t len = 0;
+	  ssize_t nread = getline(&line, &len, stdin);
+	  if (nread > 0)
+	    {
+	      if (printf(line) == -1)
+		{
+		  exit (EXIT_FAILURE);
+		}
+	      free(line);
+	    }
+	}
+      else {
+	printf ("No data within five seconds.\n");
+      }
+    }
+
+  exit (EXIT_SUCCESS);
+}
