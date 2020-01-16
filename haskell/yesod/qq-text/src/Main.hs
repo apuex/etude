bd6a2478fb50e8569b86030621f591360f97edbb
Wangxy <93236565@qq.com>
2020-01-17 00:14:36 +0800

    add quasi-quote text sample.

---
+++ haskell/yesod/qq-text/src/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE QuasiQuotes, OverloadedStrings #-}
+module Main(main) where
+
+import           Text.Shakespeare.Text
+import qualified Data.Text.Lazy.IO as TLIO
+import           Data.Text (Text)
+import           Control.Monad (forM_)
+
+data Item = Item
+  { itemName :: Text
+  , itemQty  :: Int
+  }
+
+items :: [Item]
+items =
+  [ Item "apples" 5
+  , Item "oranges" 10
+  , Item "bananas" 8
+  ]
+
+main :: IO ()
+main = forM_ items $ \item -> TLIO.putStrLn
+    [lt|You have #{show $ itemQty item} #{itemName item}.|]
