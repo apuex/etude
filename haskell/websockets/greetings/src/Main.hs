f52cd339c440f05f5cc9d8538d97a503c688724e
Wangxy <93236565@qq.com>
2020-02-02 17:42:33 +0800

    add command line opts.

--- haskell/websockets/greetings/src/Main.hs
+++ haskell/websockets/greetings/src/Main.hs
@@ -3,6 +3,12 @@ module Main
     ( main
     ) where
 
+import           System.Console.GetOpt
+import           System.IO
+import           System.Exit
+import           System.Environment
+import           Data.List
+import           Text.Printf
 import           Control.Concurrent  (forkIO)
 import           Control.Monad       (forever, unless)
 import           Control.Monad.Trans (liftIO)
@@ -13,7 +19,7 @@ import qualified Data.Text           as T
 import qualified Data.Text.IO        as T
 import qualified Network.WebSockets  as WS
 import qualified Data.ByteString.Lazy.Char8 as BL
-import Data.Aeson (FromJSON, ToJSON, decode, encode)
+import           Data.Aeson          (FromJSON, ToJSON, decode, encode)
 import           Messages
 
 app :: WS.ClientApp ()
@@ -43,4 +49,50 @@ app conn = do
     WS.sendClose conn ("Bye!" :: Text)
 
 main :: IO ()
-main = withSocketsDo $ WS.runClient "concerto" 8000 "/api/events" app
+main = do
+    progName <- getProgName
+    args     <- getArgs
+    (opts, files) <- compileOpts progName args
+    printf "host\t= %s,\nport\t= %d,\nuri\t= %s\n"
+            (hostName opts)
+            (portNumber opts)
+            (requestUri opts)
+
+    withSocketsDo $ WS.runClient
+            (hostName opts)
+            (portNumber opts)
+            (requestUri opts)
+            app
+
+data Options = Options
+    { hostName   :: String
+    , portNumber :: Int
+    , requestUri :: String
+    , printHelp  :: Bool
+    } deriving Show
+
+defaultOptions = Options
+    { hostName   = "concerto"
+    , portNumber = 8000
+    , requestUri = "/api/events"
+    , printHelp  = False
+    }
+
+options :: [OptDescr (Options -> Options)]
+options =
+   [Option ['H']    ["host-name"] (ReqArg (\ h opts -> opts { hostName = h }) "HOST") "host name or ip address"
+   , Option ['p']    ["port-number"] (ReqArg (\ p opts -> opts { portNumber = read p::Int }) "PORT") "port number"
+   , Option ['u']    ["request-uri"] (ReqArg (\ r opts -> opts { requestUri = r }) "URI") "request uri"
+   , Option ['h']    ["help"] (NoArg (\ opts -> opts { printHelp = True })) "Print this help message"]
+
+compileOpts progName argv = case getOpt Permute options argv of
+    (o,n,[]  ) -> do
+        let opts = foldl (flip id) defaultOptions o
+        if printHelp opts
+            then do hPutStrLn stderr (usageInfo header options)
+                    exitSuccess
+            else return (opts, n)
+    (_,_,errs) -> ioError (userError (concat errs ++ usageInfo header options))
+    where header = printf "Usage: %s [OPTION...] files..." progName
+
+
