3123228ef4ce41854252b614c149c0bda7f1bdcf
Wangxy <93236565@qq.com>
2020-02-03 13:39:16 +0800

    add command line options.

--- haskell/websockets/sensor-data/src/Messages.hs
+++ haskell/websockets/sensor-data/src/Messages.hs
@@ -1,35 +1,60 @@
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Messages where
 
-import           Prelude.Compat (Bool, String, Float, Maybe(Just, Nothing), Eq, Show, IO)
-import           Data.Aeson (FromJSON, ToJSON, decode, encode, Object)
+import           Data.HashMap.Strict (lookup, insert, fromList)
+import           Data.Aeson
 import qualified Data.ByteString.Lazy.Char8 as BL
 import           GHC.Generics (Generic)
 import           GHC.Int
 
-data SensorData = GreetingEvent {
+data GreetingEvent = GreetingEvent1 {
         to :: String
         , message :: String
+    } | GreetingEvent2 {
+        to1 :: String
+        , message :: String
     }
---    | AI { id :: String, analogValue :: Maybe Float }
---    | DI { id :: String, digitalValue :: Maybe Bool }
---    | Alarm {id :: String, desc :: String }
     deriving (Eq, Show, Generic)
 
 data EventEnvelope = EventEnvelope {
-        offset :: String
-        , persistenceId :: String
-        , sequenceNr :: String
-        , event :: Object
+        offset :: Maybe String
+        , persistenceId :: Maybe String
+        , sequenceNr :: Maybe String
+        , event :: Maybe GreetingEvent
     }
     deriving (Eq, Show, Generic)
 
-instance FromJSON SensorData
-instance ToJSON   SensorData
+instance FromJSON GreetingEvent where
+    parseJSON = withObject "greeting or event" $ \o -> do
+        kind <- o .: "@type"
+        case kind of
+            "type.googleapis.com/com.example.leveldb.SayHelloEvent" ->
+                GreetingEvent1 <$> o .: "to"
+                               <*> o .: "message"
+
+            "type.googleapis.com/com.example.leveldb.SayHelloEvent1" ->
+                GreetingEvent2 <$> o .: "to1"
+                               <*> o .: "message"
+
+            _                                                       ->
+                fail ("unknown kind: " ++ kind)
+
+instance ToJSON   GreetingEvent where
+    toJSON (GreetingEvent1 to message) = Object $
+        fromList [
+            "@type"   .= String "type.googleapis.com/com.example.leveldb.SayHelloEvent",
+            "to"      .= to,
+            "message" .= message
+        ]
+    toJSON (GreetingEvent2 to message) = Object $
+        fromList [
+            "@type"   .= String "type.googleapis.com/com.example.leveldb.SayHelloEvent1",
+            "to"      .= to,
+            "message" .= message
+        ]
+
 
 instance FromJSON EventEnvelope
 instance ToJSON   EventEnvelope
-
