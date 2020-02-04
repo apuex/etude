43c7dd47fdfb9d07801b44233c896cb16cc8164a
Wangxy <93236565@qq.com>
2020-02-04 23:54:59 +0800

    add simple xml sample.

---
+++ haskell/yesod/xml/simple-xml/src/Main.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+module Main(main) where
+
+import qualified Data.Map                    as M
+import           Prelude         hiding (readFile, writeFile)
+import           Data.Text (Text)
+import qualified Data.Text.Lazy.IO            as TL
+import           Text.Hamlet.XML
+import           Text.XML
+import qualified Text.XML.Stream.Render       as R
+
+main :: IO ()
+main = do
+    let prologue = Prologue [] Nothing []
+    let li = myElement (myname "bill") (M.fromList [ ("family-name", "gates") ]) []
+    let root = myElement (myname "microsoft") M.empty [NodeElement li]
+    let document = Document prologue root []
+    let rs = def { rsPretty = True }
+    let text = renderText rs document
+    TL.putStrLn text
+
+myname :: Text -> Name
+myname name = Name name (Just "http://www.microsoft.com") (Just "ns1")
+
+myElement name attrs children = Element name attrs children
+
