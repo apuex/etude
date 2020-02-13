c7141449799a6e8400b09ff887c3350411b8cd64
Wangxy <93236565@qq.com>
2020-02-13 21:07:08 +0800

    remove unused imports.

--- haskell/data-analysis/filter-expr/src/FilterExprCharsMain.hs
+++ haskell/data-analysis/filter-expr/src/FilterExprCharsMain.hs
@@ -2,13 +2,10 @@
 module Main (main) where
 
 import qualified Control.Monad        as M
-import           Data.Char
 import qualified Data.List            as L
 import qualified Data.Text.Lazy       as TL
 import qualified Data.Text.Lazy.IO    as TLIO
-import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.Set             as Set
-import           Text.Printf
 import           System.IO
 import           System.Environment
 import           CmdLine
