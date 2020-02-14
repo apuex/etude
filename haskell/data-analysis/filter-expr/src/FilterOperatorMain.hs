7166ecb9ead59dd8245187178e8cc69cc566de38
Wangxy <93236565@qq.com>
2020-02-14 14:00:25 +0800

    implement string tokenizer

--- haskell/data-analysis/filter-expr/src/FilterOperatorMain.hs
+++ haskell/data-analysis/filter-expr/src/FilterOperatorMain.hs
@@ -2,6 +2,7 @@
 module Main (main) where
 
 import qualified Control.Monad        as M
+import           Control.Monad.State
 import           Control.Monad.Trans (liftIO)
 import           Data.Char
 import qualified Data.List            as L
@@ -11,6 +12,7 @@ import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.Set             as Set
 import           GHC.IO (unIO)
 import           Text.Printf
+import           Text.Regex
 import           System.IO
 import           System.Environment
 import           CmdLine
@@ -24,10 +26,118 @@ main = do
     if null files
         then hPutStrLn stderr (header progName)
         else do
-            tokens <- M.mapM (transform opts) files
-            let charSet =
-                    L.concat tokens
-            print charSet
+            tokens <- M.mapM (withInputFile transform opts) files
+            let (_, a) = token
+                    $ TL.pack
+                    $ show
+                    $ Set.fromList
+                    $ L.concat tokens
+            TLIO.putStrLn $ TL.strip a
 
-transform :: Options -> String -> IO ([TL.Text])
-transform opts inputFile = return ["state"]
+withInputFile :: (Options -> TL.Text -> [TL.Text]) -> Options -> String -> IO [TL.Text]
+withInputFile f opts inputFile = do
+    content <- TLIO.readFile inputFile
+    return (f opts content)
+
+transform ::Options -> TL.Text -> [TL.Text]
+transform opts content = filter (\ t -> case matchRegex (mkRegex "[0-9]+") (TL.unpack t) of
+    Just _ -> False
+    _      -> True
+    )
+    $ L.concatMap (fst . tokenize)
+    $ TL.lines content
+
+chars :: Set.Set Char
+chars = Set.fromList "\n ()+.0123456789<=>[]adenorst"
+
+blankChars :: Set.Set Char
+blankChars = Set.fromList "\n\r\t "
+
+arithOpChars :: Set.Set Char
+arithOpChars = Set.fromList "+-*/"
+
+comparatorChars :: Set.Set Char
+comparatorChars = Set.fromList "<=>"
+
+getValueOpChars :: Set.Set Char
+getValueOpChars = Set.fromList "[]"
+
+numberStartChars :: Set.Set Char
+numberStartChars = Set.fromList "0123456789"
+
+numberChars :: Set.Set Char
+numberChars = Set.insert '.' numberStartChars
+
+symbolStartChars :: Set.Set Char
+symbolStartChars = Set.fromList
+    $ concat
+    [ ['_']
+    , ['A'..'Z']
+    , ['a' .. 'z']
+    ]
+
+symbolChars :: Set.Set Char
+symbolChars = Set.union symbolStartChars
+    $ Set.fromList ['0'..'9']
+
+data TokenType
+    = UnDetermined
+    | SymbolName
+    | Number
+    deriving (Eq, Show, Enum)
+
+type TokenState = State TokenType
+
+setTokenState :: TokenType -> TokenState ()
+setTokenState = put
+
+tokenState :: TokenState TokenType
+tokenState = get
+
+testState :: TokenState TokenType
+testState = do
+    setTokenState SymbolName
+    tokenState
+
+tokenize :: TL.Text -> ([TL.Text], TL.Text)
+tokenize input
+    | TL.null input = ([], TL.empty)
+    |     otherwise = loop [] input
+    where loop tokens remaining =
+            let (t, i) = token remaining
+            in if TL.null i then (tokens ++ [t], i)
+                else loop (tokens ++ [t]) i
+
+token :: TL.Text -> (TL.Text, TL.Text)
+token input
+    |       Set.member c blankChars = token tail
+    | Set.member c numberStartChars = number (TL.singleton c) tail
+    | Set.member c symbolStartChars = symbol (TL.singleton c) tail
+    |     Set.member c arithOpChars = (TL.singleton c, tail)
+    |  Set.member c comparatorChars = comparator (TL.singleton c) tail
+    |  Set.member c getValueOpChars = (TL.singleton c, tail)
+    |                     otherwise = (TL.singleton c, tail)
+    where c = TL.head input
+          tail         = TL.tail input
+
+number :: TL.Text -> TL.Text -> (TL.Text, TL.Text)
+number   o input
+    |            TL.null input = (o, input)
+    | Set.member c numberChars = number (TL.snoc o c) (TL.tail input)
+    |                otherwise = (o, input)
+    where c = TL.head input
+
+symbol :: TL.Text -> TL.Text -> (TL.Text, TL.Text)
+symbol   o input
+    |            TL.null input = (o, input)
+    | Set.member c symbolChars = symbol (TL.snoc o c) (TL.tail input)
+    |                otherwise = (o, input)
+    where c = TL.head input
+
+comparator :: TL.Text -> TL.Text -> (TL.Text, TL.Text)
+comparator o input
+    |            TL.null input = (o, input)
+    | Set.member c comparatorChars = comparator (TL.snoc o c) (TL.tail input)
+    |                otherwise = (o, input)
+    where c = TL.head input
+
