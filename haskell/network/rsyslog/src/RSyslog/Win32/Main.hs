efc4dfaf2e101f8ebfb3aab9ad989d1e9dd74516
Wangxy <xtwxy@hotmail.com>
2020-06-05 15:45:48 +0800

    add create directory if missing.

--- haskell/network/rsyslog/src/RSyslog/Win32/Main.hs
+++ haskell/network/rsyslog/src/RSyslog/Win32/Main.hs
@@ -2,6 +2,7 @@ module Main(main) where
 
 import           RSyslog.CmdLine
 import           RSyslog.UDPServer
+import           System.Directory
 import           System.Environment
 import           System.FilePath
 import           System.IO
@@ -11,6 +12,7 @@ main = do
     progName <- getProgName
     args     <- getArgs
     (opts, files) <- compileOpts progName args
+    createDirectoryIfMissing True $ logDir opts
     if console opts then serveLog "514" plainHandler
     else serveLog "514" $ fileHandler (logDir opts) "rsyslog"
 
