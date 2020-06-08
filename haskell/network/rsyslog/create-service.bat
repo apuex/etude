4e9e4a3765ae489f3fae991b43b5d1cb1a6f4404
Wangxy <xtwxy@hotmail.com>
2020-06-08 16:05:34 +0800

    add create install directory command and comments for help.

--- haskell/network/rsyslog/create-service.bat
+++ haskell/network/rsyslog/create-service.bat
@@ -1,5 +1,11 @@
 chcp 65001
 
+mkdir "%APPDATA%\local\bin\"
+copy rsyslog-udp.exe "%APPDATA%\local\bin\"
+
+REM create service with directory specified for log files
+REM for more command line options, type `rsyslog-udp.exe -h`
+
 sc create "RSyslogd" binPath="%APPDATA%\local\bin\rsyslog-udp.exe --log-dir=D:\RSyslog"
 sc config "RSyslogd" start=auto
 sc config "RSyslogd" DisplayName="RSyslog Server"
