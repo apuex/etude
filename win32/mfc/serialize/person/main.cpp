b5950e6f83bac0040686a2c8f71e1304a16abb61
Wangxy <xtwxy@hotmail.com>
2020-06-24 11:00:17 +0800

    add ansi and unicode version.

--- win32/mfc/serialize/person/main.cpp
+++ win32/mfc/serialize/person/main.cpp
@@ -12,41 +12,10 @@ int main(int argc, char* argv[]) {
   }
   
   CFile file;
-  int count = MultiByteToWideChar(CP_ACP, 0, argv[1], strlen(argv[1]), NULL, 0);
-  
-  if(count < 1) {
-    AFXDUMP(_T("Get file name length with MultiByteToWideChar failed."));
-    std::cout << "Get file name length with MultiByteToWideChar failed." << std::endl;
-    exit(4);
-  }
 
-  std::cout << "File name length is: " << count << std::endl;
-  TCHAR* fileName = new TCHAR[count + 1];
-  int size = MultiByteToWideChar(CP_ACP
-		  , 0
-		  , argv[1]
-		  , -1
-		  , fileName
-		  , count + 1
-		  );
-  DWORD err = GetLastError();
-
-  if(err != 0) {
-    AFXDUMP(_T("Convert file name with MultiByteToWideChar failed."));
-    std::cout << "Convert file name with MultiByteToWideChar failed, err: " << err << std::endl;
-    exit(3);
-  }
-
-  if(size == 0) {
-    std::cout << "Convert file name with MultiByteToWideChar failed, size: " << size << std::endl;
-    exit(2);
-  } else {
-    std::cout << "Convert file name with MultiByteToWideChar successful, size: " << size << std::endl;
-  }
-
-  if(!file.Open(fileName, CFile::modeCreate | CFile::modeWrite)) {
+  if(!file.Open(argv[1], CFile::modeCreate | CFile::modeWrite)) {
     std::cout << "Unable to open file: "
-	      << fileName << std::endl;
+	      << argv[1] << std::endl;
     AFXDUMP(_T("Unable to open file."));
     exit(1);
   }
---
+++ win32/mfc/serialize/personu/Person.cpp
@@ -0,0 +1,22 @@
+#include <Person.h>
+
+
+CPerson::CPerson() { }
+
+CPerson::CPerson(const CString& name, WORD number)
+    : m_name(name), m_number(number) { }
+
+CPerson::~CPerson() { }
+
+void CPerson::Serialize(CArchive& archive) {
+  CObject::Serialize(archive);
+
+  if(archive.IsStoring()) {
+    archive << m_name << m_number;
+  } else {
+    archive >> m_name >> m_number;
+  }
+}
+
+IMPLEMENT_SERIAL(CPerson, CObject, VERSIONABLE_SCHEMA | 0)
+
