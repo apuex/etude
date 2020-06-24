46c5b7f26fc4ae93ab02f048619311ba40e95394
Wangxy <xtwxy@hotmail.com>
2020-06-24 13:52:41 +0800

    add CString serialize test.

---
+++ win32/mfc/serialize/cstring/main.cpp
@@ -0,0 +1,31 @@
+#include <afx.h>
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+
+  if(argc < 2) {
+    std::cout << "Usage:" << std::endl
+	      << argv[0]  << " <file>"
+	      << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  CFile file;
+
+  if(!file.Open(argv[1], CFile::modeCreate | CFile::modeWrite)) {
+    std::cout << "Unable to open file: "
+	      << argv[1] << std::endl;
+    AFXDUMP(_T("Unable to open file."));
+    exit(1);
+  }
+
+  CArchive archive(&file, CArchive::store);
+
+  CString person("Wangxy");
+
+  archive << person;
+
+  return EXIT_SUCCESS;
+}
+
