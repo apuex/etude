d24a24ad2f68b783d3d6671bb61678b9449de971
Wangxy <xtwxy@hotmail.com>
2020-12-28 12:42:03 +0800

    add set system clock test. disable auto-sync from internet firstgit add set-system-clock.c git add set-system-clock.c

---
+++ linux/sys-api/stime/set-system-clock.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <linux/rtc.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main() {
+  time_t clock;
+  time_t one_year_after;
+  struct tm mile_stone = {0};
+  double seconds;
+
+  int fd;
+  struct rtc_time rt;
+
+  time(&clock);
+  memcpy(&mile_stone, localtime(&clock), sizeof(struct tm));
+
+  mile_stone.tm_year = 1 + mile_stone.tm_year;
+
+  one_year_after = mktime(&mile_stone);
+  seconds = difftime(clock, one_year_after);
+
+  printf("%.f seconds 1 year from now in the current timezone.\n", seconds);
+
+  printf("before stime(): %lu - %lu.\n", clock, one_year_after);
+  if(0 == stime(&one_year_after)) {
+    printf("set system time success(%d).\n", errno);
+  } else {
+    printf("set system time failure(%d).\n", errno);
+  }
+  time(&clock);
+  printf("after stime(): %lu - %lu.\n", clock, one_year_after);
+/*
+  rt.tm_sec = mile_stone.tm_sec;
+  rt.tm_min = mile_stone.tm_min;
+  rt.tm_hour = mile_stone.tm_hour;
+  rt.tm_mday = mile_stone.tm_mday;
+  rt.tm_mon = mile_stone.tm_mon;
+  rt.tm_year = mile_stone.tm_year;
+
+  fd = open("/dev/rtc", O_RDONLY);
+  ioctl(fd, RTC_SET_TIME, &rt);
+  close(fd);
+*/
+  return 0;
+}
