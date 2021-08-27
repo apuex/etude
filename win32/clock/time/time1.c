1c55007f548da83bb71640ca89dd8c7669da93fc
Wangxy <xtwxy@hotmail.com>
2021-08-27 15:02:35 +0800

    remove unused MACRO definitions.

--- win32/clock/time/time1.c
+++ win32/clock/time/time1.c
@@ -4,8 +4,6 @@
 // #include <time.h> // this is a MUST to make time(time_t *) right!!!
 #include <windows.h>
 
-#define EPOCH_DIFF 116444736000000000 // times 100 nano seconds.
-
 int main(int argc, char *argv[]) {
   printf("sizeof(long)   = %zu\n", sizeof(long));
   printf("sizeof(size_t) = %zu\n", sizeof(size_t));
