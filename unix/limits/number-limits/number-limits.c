f82b07fe57d623c4534f1e347cb914d88fd4b136
Wangxy <xtwxy@hotmail.com>
2021-08-27 16:39:57 +0800

    add number sizes.

---
+++ unix/limits/number-limits/number-limits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main(int argc, char *argv[]) {
+  printf("sizeof(char)      = %zu\n", sizeof(char));
+  printf("sizeof(short)     = %zu\n", sizeof(short));
+  printf("sizeof(int)       = %zu\n", sizeof(int));
+  printf("sizeof(long)      = %zu\n", sizeof(long));
+  printf("sizeof(long long) = %zu\n", sizeof(long long));
+  printf("sizeof(float)     = %zu\n", sizeof(float));
+  printf("sizeof(double)    = %zu\n", sizeof(double));
+  printf("sizeof(size_t)    = %zu\n", sizeof(size_t));
+  printf("sizeof(time_t)    = %zu\n", sizeof(time_t));
+  return 0;
+}
