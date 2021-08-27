5b39982c102a3fff6012457b990a6ed35e93a025
Wangxy <xtwxy@hotmail.com>
2021-08-27 12:12:34 +0800

    add SYSTEMTIME => time_t conversion.

--- win32/clock/timeval/timeval.c
+++ win32/clock/timeval/timeval.c
@@ -1,17 +1,31 @@
 #define UNICODE
 #include <windows.h>
 #include <stdio.h>
+#include <stdint.h>
 
+#define EPOCH_DIFF 116444736000000000 // times 100 nano seconds.
 
 int main(int argc, char *argv[]) {
 	FILETIME ft;
 	SYSTEMTIME st;
+	uint64_t nanosecs;
+	time_t ts;
 	TIME_ZONE_INFORMATION tz;
 	GetTimeZoneInformation(&tz);
 	GetSystemTimeAsFileTime(&ft);
+	time(&ts);
+	nanosecs = ((uint64_t)ft.dwHighDateTime << 32) | ft.dwLowDateTime;
+	wprintf(TEXT("sizeof(long) = %zu\n"), sizeof(long));
+	wprintf(TEXT("nanosecs = %zu - time_t = %zu => %zu\n"),
+		       nanosecs / 10000000,
+		       (ts ),
+		       (nanosecs / 10000000 - (ts ))
+		       );
 	wprintf(TEXT("FILETIME(epoch 1601 AD): %lu.%lu\n"), ft.dwHighDateTime, ft.dwLowDateTime);
 	
 	GetSystemTime(&st);
+        ts = (nanosecs - EPOCH_DIFF) / 10000000;
+	wprintf(TEXT("time_t = %zu\n"), ts);
 	wprintf(TEXT("SYSTEMTIME(UTC): %04d-%02d-%02d %02d:%02d:%02d.%03d, Weekday: %d\n"),
 			st.wYear,
 			st.wMonth,
@@ -35,7 +49,7 @@ int main(int argc, char *argv[]) {
 			st.wMilliseconds,
 			st.wDayOfWeek
 			);
-
+	nanosecs = ((uint64_t)ft.dwHighDateTime << 32) | ft.dwLowDateTime;
 
 	return 0;
 }
