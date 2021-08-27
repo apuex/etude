041a3dc2764918ac7f26a0529b652a234ce36cbf
Wangxy <xtwxy@hotmail.com>
2021-08-27 17:25:22 +0800

    test on mipsel.

--- unix/clock/timeval/timeval.c
+++ unix/clock/timeval/timeval.c
@@ -1,17 +1,32 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <sys/time.h>
 #include <time.h>
 
 int main(int argc, char *argv[]) {
+  time_t mask = -1;
   struct timeval tv;
   struct tm *timeinfo;
   char buffer[32];
   gettimeofday(&tv, NULL);
   timeinfo = localtime(&tv.tv_sec);
   strftime(buffer, sizeof(buffer), "%F %T", timeinfo);
-  printf("%ld.%06ld => %s\n", tv.tv_sec, tv.tv_usec, buffer);
-  printf("%ld.%06lds = %ldms\n", tv.tv_sec, tv.tv_usec, 
-         (tv.tv_sec * 1000 + (tv.tv_usec / 1000)));
+  
+  // cannot work (msecs = 0) on mipsel(loongson) process.
+  // uint64_t msecs = (tv.tv_sec * (uint64_t)1000 + (tv.tv_usec / 1000));
+  printf("%zu.%06zu  => [%s]\n", 
+         tv.tv_sec, 
+         tv.tv_usec, 
+         buffer);
+  printf("%zu.%06zus => %zums(64bit)\n", 
+         tv.tv_sec, 
+         tv.tv_usec, 
+         (tv.tv_sec * (uint64_t)1000 + (tv.tv_usec / 1000)));
+  time_t msecs32 = mask & (tv.tv_sec * (uint64_t)1000 + (tv.tv_usec / 1000));
+  printf("%zu.%06zus => %zums(32bit)\n", 
+         tv.tv_sec, 
+         tv.tv_usec,
+         msecs32);
 
   return 0;
 }
--- unix/limits/number-limits/number-limits.c
+++ unix/limits/number-limits/number-limits.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -9,6 +10,7 @@ int main(int argc, char *argv[]) {
   printf("sizeof(int)       = %zu\n", sizeof(int));
   printf("sizeof(long)      = %zu\n", sizeof(long));
   printf("sizeof(long long) = %zu\n", sizeof(long long));
+  printf("sizeof(uint64_t)  = %zu\n", sizeof(uint64_t));
   printf("sizeof(float)     = %zu\n", sizeof(float));
   printf("sizeof(double)    = %zu\n", sizeof(double));
   printf("sizeof(size_t)    = %zu\n", sizeof(size_t));
