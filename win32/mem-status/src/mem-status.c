5fd8a5116511cb6096d41c1b580ebffbbe15b97c
Wangxy <xtwxy@hotmail.com>
2021-09-18 17:59:58 +0800

    print memory status.

--- win32/mem-status/src/mem-status.c
+++ win32/mem-status/src/mem-status.c
@@ -1,13 +1,43 @@
 #include <windows.h>
+#include <stdio.h>
 
-int WINAPI WinMain(
-    HINSTANCE hInstExe,
-    HINSTANCE HInstPre,
-    PWSTR pszCmdLine,
-    int nCmdShow) {
+int main(int argc, char* argv[]) {
+
+#define size 1024+1
+  WCHAR buffer[size];
 
   MEMORYSTATUS ms;
   GlobalMemoryStatus(&ms);
+
+  LPWSTR pMessage = L"%1!*s! %3!zu!%4%n"
+                    L"%5!*s! %7!zu!%n"
+                    L"%8!*s! %10!zu!%n"
+                    L"%11!*s! %13!zu!%n"
+                    L"%14!*s! %16!zu!%n"
+                    L"%17!*s! %19!zu!%n"
+                    L"%20!*s! %21!zu!";
+    DWORD_PTR pArgs[] = {
+           (DWORD_PTR)24, (DWORD_PTR)L"dwMemoryLoad:", ms.dwMemoryLoad, (DWORD_PTR)L"%%",
+           (DWORD_PTR)24, (DWORD_PTR)L"dwTotalPhys:", ms.dwTotalPhys,
+           (DWORD_PTR)24, (DWORD_PTR)L"dwAvailPhys:", ms.dwAvailPhys,
+           (DWORD_PTR)24, (DWORD_PTR)L"dwTotalPageFile:", ms.dwTotalPageFile,
+           (DWORD_PTR)24, (DWORD_PTR)L"dwAvailPageFile:", ms.dwAvailPageFile,
+           (DWORD_PTR)24, (DWORD_PTR)L"dwTotalVirtual:", ms.dwTotalVirtual,
+           (DWORD_PTR)24, (DWORD_PTR)L"dwAvailVirtual:", ms.dwAvailVirtual
+    };
+
+  if (!FormatMessage(FORMAT_MESSAGE_FROM_STRING | FORMAT_MESSAGE_ARGUMENT_ARRAY,
+                     pMessage, 
+                     0,
+                     0,
+                     buffer, 
+                     size, 
+                     (va_list*)pArgs)) {
+    wprintf(L"Format message failed with 0x%x\n", GetLastError());
+  } else {
+    //MessageBox(NULL, buffer, L"Memory Status", MB_OK);
+    wprintf(L"Memory Status:\n%s\n", buffer);
+  }
 
   return 0;
 }
