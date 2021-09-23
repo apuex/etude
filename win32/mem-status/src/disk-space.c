7adc30f28643144f73ccb2c5f9f21b687c1381e7
Wangxy <xtwxy@hotmail.com>
2021-09-23 12:28:02 +0800

    add get all hard disk volume space sample

--- win32/mem-status/src/disk-space.c
+++ win32/mem-status/src/disk-space.c
@@ -5,9 +5,29 @@ int main(int argc, char* argv[]) {
   ULARGE_INTEGER available;
   ULARGE_INTEGER total;
   ULARGE_INTEGER free;
+  WCHAR path[MAX_PATH];
 
-  GetDiskFreeSpaceEx(NULL, &available, &total, &free);
+  for (int i = 0; i != argc; ++i) {
+    printf("argv[%d] = %s\n", i, argv[i]);
+  }
+  if (argc > 1) {
+    MultiByteToWideChar( GetACP()
+                       , MB_PRECOMPOSED
+                       , argv[1]
+                       , strlen(argv[1]) + 1 // or, -1 if null terminated.
+                       , path
+                       , ARRAYSIZE(path)
+                       );
 
+    wprintf(L"             path: %s\n", path);
+    wprintf(L"             path: %s\n", path);
+    GetDiskFreeSpaceEx(path, &available, &total, &free);
+  }
+  else {
+    GetDiskFreeSpaceEx(NULL, &available, &total, &free);
+  }
+
+  wprintf(L"        code page: %u\n", GetACP());
   wprintf(L"            total: %zu\n", total.QuadPart);
   wprintf(L"             free: %zu\n", free.QuadPart);
   wprintf(L"available to user: %zu\n", available.QuadPart);
---
+++ win32/mem-status/src/volume.c
@@ -0,0 +1,62 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  HANDLE volHandle = INVALID_HANDLE_VALUE;
+  BOOL found = TRUE;
+  WCHAR VolumeName[MAX_PATH] = L"";
+  WCHAR  DeviceName[MAX_PATH] = L"";
+  WCHAR  PathName[MAX_PATH + 1] = L"";
+  WCHAR  DriverPathName[MAX_PATH + 1] = L"";
+  PWCHAR NameIdx   = NULL;
+
+  DWORD  CharCount = ARRAYSIZE(PathName);
+  size_t index = 0;
+
+  ULARGE_INTEGER available;
+  ULARGE_INTEGER total;
+  ULARGE_INTEGER free;
+
+  volHandle = FindFirstVolume(VolumeName, sizeof(VolumeName) / sizeof(WCHAR));
+  if (volHandle != INVALID_HANDLE_VALUE) {
+    for (
+      ;
+      TRUE == found;
+      found = FindNextVolume(volHandle, VolumeName, sizeof(VolumeName) / sizeof(WCHAR))
+      ) {
+      wprintf(L"Volume Name: %s\n", VolumeName);
+      GetVolumePathNamesForVolumeName(VolumeName, PathName, CharCount, &CharCount);
+
+      index = wcslen(VolumeName) - 1;
+      VolumeName[index] = L'\0';
+      QueryDosDevice(&VolumeName[4], DeviceName, ARRAYSIZE(DeviceName));
+      wprintf(L"Device Name: %s\n", DeviceName);
+      wprintf(L"Path Names:\n");
+      for (NameIdx = PathName;
+        NameIdx[0] != L'\0';
+        NameIdx += wcslen(NameIdx) + 1)
+      {
+        if (':' != NameIdx[1]) {
+          wsprintf(DriverPathName, L"%s:", NameIdx);
+        }
+        else {
+          wsprintf(DriverPathName, L"%s", NameIdx);
+        }
+        wprintf(L"  %s\n", DriverPathName);
+        if (0 == wcsncmp(L"\\Device\\HarddiskVolume", DeviceName, 22))
+        {
+          if (GetDiskFreeSpaceEx(DriverPathName, &available, &total, &free)) {
+            wprintf(L"              total: %zu\n", total.QuadPart);
+            wprintf(L"               free: %zu\n", free.QuadPart);
+            wprintf(L"  available to user: %zu\n", available.QuadPart);
+          }
+        }
+      }
+      wprintf(L"\n");
+    }
+    FindVolumeClose(volHandle);
+  }
+
+  return 0;
+}
+
