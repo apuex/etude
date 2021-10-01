e35651a7ed8837d4575236f84faa250423e8622d
Wangxy <xtwxy@hotmail.com>
2021-10-01 18:41:18 +0800

    compile with windows 2003 sdk.

---
+++ win32/trivial/build-w2003sdk.bat
@@ -0,0 +1,10 @@
+cmake -G "NMake Makefiles" ^
+ -DCMAKE_C_FLAGS_DEBUG="/MD /GS" ^
+ -DCMAKE_C_FLAGS_RELEASE="/MD /GS /O2 /DNDEBUG" ^
+ -DCMAKE_EXE_LINKER_FLAGS="/machine:amd64" ^
+ -DCMAKE_MODULE_LINKER_FLAGS="/machine:amd64" ^
+ -DCMAKE_STATIC_LINKER_FLAGS="/machine:amd64" ^
+ -DCMAKE_BUILD_TYPE=Release ^
+ -DCMAKE_INSTALL_PREFIX=C:\usr ^
+ ..
+
