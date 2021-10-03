0687ac721c5c9217ede406ba8214c17cec34ec7b
Wangxy <xtwxy@hotmail.com>
2021-10-03 15:38:35 +0800

    add print states in test.

--- data-structure/ring-buffer/ring-buffer-test.cpp
+++ data-structure/ring-buffer/ring-buffer-test.cpp
@@ -11,30 +11,108 @@ int main(int argc, char* argv[]) {
   char_buffer buffer(64);
   char input[32] = {"hello, world!"};
   char output[32] = {0};
- 
-  cout << "writting [" << input << "] to buffer" << endl; 
+
+  cout << "[BEFORE]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl;
+  cout << "writting [" << input << "] to buffer..";
   len = buffer.write(input, sizeof(input));
   cout << len << " bytes written." << endl;
+  cout << "[AFTER]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl
+     << endl;
 
-  cout << "writting [" << input << "] to buffer" << endl; 
+  cout << "[BEFORE]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl;
+  cout << "reading bytes from buffer to output..";
+  len = buffer.read(output, sizeof(output));
+  cout << len << " bytes read: [" << output << "]" << endl;
+  cout << "[AFTER]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl
+     << endl;
+
+  cout << "[BEFORE]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl;
+  cout << "writting [" << input << "] to buffer..";
   len = buffer.write(input, sizeof(input));
   cout << len << " bytes written." << endl;
+  cout << "[AFTER]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl
+     << endl;
 
-  cout << "writting [" << input << "] to buffer" << endl; 
+  cout << "[BEFORE]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl;
+  cout << "reading bytes from buffer to output..";
+  len = buffer.read(output, sizeof(output));
+  cout << len << " bytes read: [" << output << "]" << endl;
+  cout << "[AFTER]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl
+     << endl;
+
+  cout << "[BEFORE]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl;
+  cout << "writting [" << input << "] to buffer..";
   len = buffer.write(input, sizeof(input));
   cout << len << " bytes written." << endl;
+  cout << "[AFTER]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl
+     << endl;
 
-  cout << "reading bytes from buffer to output" << endl; 
+  cout << "[BEFORE]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl;
+  cout << "reading bytes from buffer to output..";
   len = buffer.read(output, sizeof(output));
   cout << len << " bytes read: [" << output << "]" << endl;
-
-  cout << "reading bytes from buffer to output" << endl; 
-  len = buffer.read(output, sizeof(output));
-  cout << len << " bytes read: [" << output << "]" << endl;
-
-  cout << "reading bytes from buffer to output" << endl; 
-  len = buffer.read(output, sizeof(output));
-  cout << len << " bytes read: [" << output << "]" << endl;
+  cout << "[AFTER]"
+     << ": buffer_size=" << buffer.buffer_size()
+     << ", element_count=" << buffer.element_count()
+     << ", read_pos=" << buffer.read_pos()
+     << ", write_pos=" << buffer.write_pos()
+     << endl
+     << endl;
 
   return 0;
 }
--- data-structure/ring-buffer/ring-buffer.hpp
+++ data-structure/ring-buffer/ring-buffer.hpp
@@ -10,39 +10,44 @@ class ring_buffer {
  public:
   typedef T value_type;
   ring_buffer(size_t size)
-     : buffer_size(size)
-     , buffer(new value_type[size])
-     , element_count(0)
-     , rd_pos(0)
-     , wr_pos(0) 
+     : _buffer_size(size)
+     , _buffer(new value_type[size])
+     , _element_count(0)
+     , _rd_pos(0)
+     , _wr_pos(0) 
      { }
-  virtual ~ring_buffer() { delete[] buffer; }
+  virtual ~ring_buffer() { delete[] _buffer; }
 
   size_t read(value_type* rbuf, size_t to_read) {
-    if(0 == element_count) return 0;
+    if(0 == _element_count) return 0;
     else {
       size_t i = 0;
-      for(i = 0; i != to_read && i != element_count; ++i) {
-        *(rbuf + i) = *(buffer + rd_pos); ++rd_pos;
-        if(buffer_size == rd_pos) rd_pos = 0;
+      for(i = 0; i != to_read && i != _element_count; ++i) {
+        *(rbuf + i) = *(_buffer + _rd_pos); ++_rd_pos;
+        if(_buffer_size == _rd_pos) _rd_pos = 0;
       }
-      element_count -= i;
+      _element_count -= i;
       return i;
     }
   }
 
   size_t write(const value_type* const wbuf, size_t to_write) {
-    if(buffer_size == element_count) return 0;
+    if(_buffer_size == _element_count) return 0;
     else {
       size_t i = 0;
-      for(i = 0; i != to_write && i != (buffer_size - element_count); ++i) {
-        *(buffer + wr_pos) = *(wbuf + i); ++wr_pos;
-        if(buffer_size == wr_pos) wr_pos = 0;
+      for(i = 0; i != to_write && i != (_buffer_size - _element_count); ++i) {
+        *(_buffer + _wr_pos) = *(wbuf + i); ++_wr_pos;
+        if(_buffer_size == _wr_pos) _wr_pos = 0;
       }
-      element_count += i;
+      _element_count += i;
       return i;
     }
   }
+
+  size_t buffer_size() const { return _buffer_size; }
+  size_t element_count() const { return _element_count; }
+  size_t read_pos() const { return _rd_pos; }
+  size_t write_pos() const { return _wr_pos; }
 
  private:
   ring_buffer(const ring_buffer& rv) { }
@@ -50,11 +55,11 @@ class ring_buffer {
   bool operator==(const ring_buffer& rv) const { return false; }
 
  private:
-  const size_t buffer_size;
-  value_type* buffer;
-  size_t element_count;
-  size_t rd_pos;
-  size_t wr_pos;
+  const size_t _buffer_size;
+  value_type* _buffer;
+  size_t _element_count;
+  size_t _rd_pos;
+  size_t _wr_pos;
 };
 
 }
