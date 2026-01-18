44ea9ef2fd510da22444f20a4b8f425df764571f
Wangxy <xtwxy@hotmail.com>
2026-01-18 12:44:20 +0800

    add 6+3 format sample.

--- c/stdint/tests/printf-format.cpp
+++ c/stdint/tests/printf-format.cpp
@@ -2,6 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct fmt_desc {
+  const char name[64];
+  int m;
+  int c;
+};
+
+void format_desc(fmt_desc* fmt) {
+  printf(fmt->name, fmt->m, fmt->c);
+}
+
+void format_module(int n) {
+  fmt_desc descs[] =
+  { { "module=%02d, signal=%09d\n", n, 8352001 + ( (n - 1) * 3) }
+  , { "module=%02d, signal=%09d\n", n, 8352002 + ( (n - 1) * 3) }
+  , { "module=%02d, signal=%09d\n", n, 8352003 + ( (n - 1) * 3) }
+  };
+
+  for(size_t i = 0; i != (sizeof(descs)/sizeof(fmt_desc)); ++i) {
+    format_desc(&descs[i]);
+  }
+}
+
 int main(int argc, char* argv[]) {
   if(1 == argc)
   {
@@ -10,7 +32,8 @@ int main(int argc, char* argv[]) {
   }
 
   int n = atoi(argv[1]);
-  printf("module%02d\n", n);
+  format_module(n);
 
   return EXIT_SUCCESS;
 }
+
