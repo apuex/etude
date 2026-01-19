03a564a95d8960abb7b5b113ba3ec1f45bf00475
Wangxy <xtwxy@hotmail.com>
2026-01-19 20:09:04 +0800

    update file description in version information

--- win32/clock/erlang/src/version.h
+++ win32/clock/erlang/src/version.h
@@ -3,7 +3,7 @@
 
 #define VER_FILETYPE                 VFT_APP
 #define VER_FILESUBTYPE              VFT2_UNKNOWN
-#define VER_FILEDESCRIPTION_STR     "Wall clock and Steady clock in millisecond."
+#define VER_FILEDESCRIPTION_STR     "Wall clock and Steady clock in Unit of second, millisecond, microsecond, and nanosecond."
 #define VER_FILECOMMENT_STR         "by Wangxy\n"
 #define VER_INTERNALNAME_STR        "clock Windows Native port for Erlang by Wangxy.\0"
 #define VER_ORIGINALFILENAME_STR    "clock.exe"
